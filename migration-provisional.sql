093e4c0955cf76255eade58f3a8388e66c9637ca
Kim <kimhoehe@web.de>
2026-09-08 18:45:31 +0200

    Migration fuer die Spalte provisional

--- migration-provisional.sql
+++ migration-provisional.sql
@@ -1,2 +1,2 @@
--- Vorlaeufige Kategorien: stammen von Buchungsportalen, bis jemand sie prueft.
+-- Kennzeichnet Kategorien, deren Namen von einem Buchungsportal stammen.
 ALTER TABLE room_types ADD COLUMN provisional INTEGER NOT NULL DEFAULT 0;

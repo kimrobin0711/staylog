acbf61e17e1dcf5a847784fb6f0e35c146938389
Kim <kimhoehe@web.de>
2026-09-08 21:38:21 +0200

    Namen beim Speichern vereinheitlichen, Doppelte zusammenfassen

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Die Version steht in version.json und wird beim Packen gesetzt.
-const VERSION = '20260908-213312';
+const VERSION = '20260908-213801';
 const SHELL = 'staylog-' + VERSION;
 const FILES = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest'];
 

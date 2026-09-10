6c4c3761f7835f6d5f7be23b3e6af071a09d80a0
Kim <kimhoehe@web.de>
2026-09-10 21:40:19 +0200

    Doppeltes Zimmer am Namensende entfernt

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Die Version steht in version.json und wird beim Packen gesetzt.
-const VERSION = '20260910-213625';
+const VERSION = '20260910-213929';
 const SHELL = 'staylog-' + VERSION;
 const FILES = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest'];
 
